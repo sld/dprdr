cc0ae081c444f13c02b289755dfb2f83f7d27129
Idris <sld7700@gmail.com>
2013-10-19 04:58:02 +0400

    rails_apps_composer: initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Dpreader::Application.config.session_store :cookie_store, key: '_dpreader_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Dpreader::Application.config.session_store :active_record_store
