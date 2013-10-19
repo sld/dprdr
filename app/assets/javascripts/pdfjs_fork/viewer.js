2be206a7726059c0547312c1b985001793016439
Idris <sld7700@gmail.com>
2013-10-19 15:26:15 +0400

    csrf fixed and updated viewer.js and viewer.html.erb

--- app/assets/javascripts/pdfjs_fork/viewer.js
+++ app/assets/javascripts/pdfjs_fork/viewer.js
@@ -4651,19 +4651,16 @@ window.addEventListener('pagechange', function pagechange(evt) {
 
     var book_id = document.getElementById("book_id_for_pdfjs").getAttribute('data-value')
     var url = "/api/v1/books/" + book_id + "/save_page";
-    var params = "page="+page;
+    var params = "page="+page+"&id="+book_id;
     var http = new XMLHttpRequest();
+    var token = $("meta[name='csrf-token']").attr("content");
     http.open("POST", url, true);
 
     //Send the proper header information along with the request
+    http.setRequestHeader("X-CSRF-Token", token);
     http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     http.setRequestHeader("Content-length", params.length);
     http.setRequestHeader("Connection", "close");
-    http.onreadystatechange = function() {//Call a function when the state changes.
-        if(http.readyState == 4 && http.status == 200) {
-            alert(http.responseText);
-        }
-    }
     http.send(params);
 
 
