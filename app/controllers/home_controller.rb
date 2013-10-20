875176732db8ed26bf586e11b1c504134fac07fd
Idris <sld7700@gmail.com>
2013-10-21 01:41:54 +0400

    Added for rumble screenshot bot

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    if params[:screenshot] == "true"
+      filepath = "#{Rails.root}/public/screenshoot-dpreader.png"
+      render :text => File.open(filepath, 'rb').read,
+             :status => 200, :content_type => 'image/png'
+
+    end
   end
 end
