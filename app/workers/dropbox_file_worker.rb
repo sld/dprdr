bfc8c39f9ed8a142693e76c9ebfec35d6dbc976d
Idris <sld7700@gmail.com>
2013-11-20 22:00:28 +0400

    Changed save! to save in DropboxFileWorker to deal with uniqueness validation in DropboxFile

--- app/workers/dropbox_file_worker.rb
+++ app/workers/dropbox_file_worker.rb
@@ -7,6 +7,10 @@ class DropboxFileWorker
 
   def perform book_id, metadata
     book = Book.find book_id
+
+    book.update_column :name, metadata['path'].split("/").last
+    book.update_column :dropbox_processing, true
+
     dropbox_file = book.build_dropbox_file
 
     dropbox_file.mime_type = metadata['mime_type']
@@ -15,9 +19,12 @@ class DropboxFileWorker
     dropbox_file.revision = metadata['revision']
     dropbox_file.size = metadata['size']
 
-    book.update_column :name, metadata['path'].split("/").last
-
-    dropbox_file.save!
+    if dropbox_file.save
+      book.update_column :dropbox_processing, false
+    else
+      p ["Error on dropbox_file save", dropbox_file.errors.messages]
+      book.delete
+    end
   rescue Exception => e
     Honeybadger.notify e
     p e
